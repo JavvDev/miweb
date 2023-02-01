84e1ba65db3fbf3a06b45e191c9d9ea23f7e70dd
JavvDev <jhonvielma11@gmail.com>
2023-02-01 02:12:56 -0400

    changed this file for adapting it to an exercise of the bootcamp

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,22 +1,39 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
-import Mensaje from './Mensaje'
 
-const App = () => {
+const Title = ({course}) => <h1>{course}</h1>
 
-  // aqui se puede ejecutar todo el codigo de javascript que sea necesario
 
+const Element = ({part, exercise}) => {
   return (
-    <div>
-        <Mensaje color="purple" message='Rebeca'/>
-        <Mensaje color="pink" message='Vanessa'/>
-        <Mensaje color="blue" message='Laleska'/>
-        <Mensaje color="brown" message='Mafer'/>
-    </div>
+  <p>
+  {part} {exercise}
+  </p>
   )
 }
 
+const Total = ({exercise1, exercise2, exercise3}) => <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Title course={course}/>
+      <Element part={part1} exercise={exercises1}/>
+      <Element part={part2} exercise={exercises2}/>
+      <Element part={part3} exercise={exercises3}/>
+      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
+    </>
+  )
+}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+<App />,
+document.getElementById('root'))
